6c4b14eea9a1a1ebdc99dce0121bb6e21f0ead03
Andre Grassi <andregrassi.j@gmail.com>
2022-08-03 20:10:13 -0300

    Adds initial files. Light styling and just the header in the page

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+// Stylesheet:
+import './styles.css'
+
+function Header() {
+  return ( 
+    <header>
+      <h1>
+        Click<span>Fast</span>.
+      </h1>
+    </header>
+   )
+}
+
+export default Header
---
+++ src/pages/Index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import './App.css'
+
+// Components:
+import Header from '../components/Header'
+
+function Index() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default Index
