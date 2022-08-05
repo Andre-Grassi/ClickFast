1fe964783e457ad466a431445ce8a7d272ef11de
Andre Grassi <andregrassi.j@gmail.com>
2022-08-05 18:58:25 -0300

    :rocket: Add event listener to button that initialize counting the CPS in Main.tsx Add event listener to button when it's clicked Add ```handleClick()``` function. Add variables that are used in ```handleClick()``` function

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,36 @@
 import '../styles.css'
 
 function Main() {
+  // Initialize variables of handleClick function
+  let time = 2000     // Time in milliseconds that the timer will run
+  let start = false   // Boolean value indicating start of time 
+  let counter = 0     // Number of clicks
+  
+  // Count clicks
+  function handleClick() {
+    
+    // Activate the timer if this is the first click
+    if (start == false) {
+      counter = 0
+
+      setTimeout(function(){
+        console.log(counter);
+        start = false
+      }
+      , time) }
+
+    // After first click, it won't reset the timer
+    start = true
+
+    // Add the click to counter
+    ++counter
+  }
+
   return (
     <main>
       <h2>Test the <span>speed</span> you click</h2>
       <div className="btn">
-        <button onClick={() => console.log('clicked')}>Click</button>
+        <button onClick={handleClick}>Click</button>
       </div>
     </main>
   );
