abe500b44e2f319b8cec78d5473a6b020713067f
Andre Grassi <andregrassi.j@gmail.com>
2022-08-06 20:58:30 -0300

    :gear: Add config button. Add container that contains the new ```button``` and the ```h2```. Add button image (configwheel.png). Reset button style in ```global.css```.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -5,9 +5,12 @@ import handleClick from './handleClick'
 function Main() {
   return (
     <main>
-      <h2>Test the <span>speed</span> you click</h2>
+      <div className="container">
+        <button id='config' aria-label='config button'></button>
+        <h2>Test the <span>speed</span> you click</h2>
+      </div>
       <div className="btn">
-        <button onClick={handleClick}>Click</button>
+        <button id='counter' onClick={handleClick}>Click</button>
       </div>
     </main>
   );
