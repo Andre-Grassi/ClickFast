94d4538a277dd48ad48099e6707f3f6586747d44
Andre Grassi <andregrassi.j@gmail.com>
2022-08-07 03:27:34 -0300

    Add config-container with slider inside it. Add config container, which has a ```h3``` and an ```input type="range"``` inside. Create ```range.css```, a file to style the slider. Add ```@import url('range.css');``` inside ```styles.css```.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,11 +3,16 @@ import '../styles.css'
 import handleClick from './handleClick'
 
 function Main() {
+  let active = true
   return (
     <main>
       <div className="container">
         <button id='config' aria-label='config button'></button>
         <h2>Test the <span>speed</span> you click</h2>
+        {active && <section id='config-container'>
+          <h3>Set the interval to count</h3>
+          <input type="range" name="interval of time to count" min="1" max="10" step="1"/>
+        </section>}
       </div>
       <div className="btn">
         <button id='counter' onClick={handleClick}>Click</button>
